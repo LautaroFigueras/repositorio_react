a8bc154c23e9d5c9002dde4669c21638d8a00e90
Lautaro Figueras <lautaromfigueras@gmail.com>
2021-11-30 23:11:55 -0300

    navbar

---
+++ prueba-react/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function NavBar() {
+    return(
+        <div>
+                <ul> 
+                <li> 
+                <a>Inicio </a>
+                </li>
+                <li> 
+                <a>Imagenes </a>
+                </li>
+                <li> 
+                <a>Contacto </a>
+                </li>
+                </ul>
+        </div>
+
+    )
+
+
+}
+export default NavBar
