faaf8e64677c6ae0dbf73253d9642b553bd89734
Lautaro Figueras <lautaromfigueras@gmail.com>
2021-12-17 00:06:34 -0300

    nav bar

--- prueba-react/src/components/NavBar.jsx
+++ prueba-react/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import CartWidget from './CartWidget'
 
 
 
@@ -9,13 +10,14 @@ import Nav from 'react-bootstrap/Nav'
 function NavBar() {
     return(
         <>
-             <Navbar bg="dark" variant="dark">
+             <Navbar bg="light" variant="light">
                         <Container>
                         <Navbar.Brand href="#home">Magic</Navbar.Brand>
                         <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Remeras</Nav.Link>
-                        <Nav.Link href="#pricing">Cuadros</Nav.Link>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Remeras</Nav.Link>
+                            <Nav.Link href="#pricing">Cuadros</Nav.Link>
+                            <CartWidget />
                         </Nav>
                         </Container>
             </Navbar>
