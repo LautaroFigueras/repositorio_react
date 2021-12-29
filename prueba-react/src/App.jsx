d20615d46c285d999cca898b2447736d90581f01
Lautaro Figueras <lautaromfigueras@gmail.com>
2021-12-28 22:55:53 -0300

    app

--- prueba-react/src/App.jsx
+++ prueba-react/src/App.jsx
@@ -1,20 +1,21 @@
 // import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 import NavBar from './components/NavBar'
+import Cart from '../context/Cart';
+import CartContextProvider from '../context/CartContext';
+
 function App() {
   const estilos = {backgroundColor: 'blue'}
 
-  const Evento = () =>{
-    console.log('Hola') 
-  }
-
+ 
   return (
       <div className="App" style={estilos} onClick={Evento}>
-
+        <CartContextProvider>
         <NavBar/>
         <ItemListContainer />
-
+        </CartContextProvider>
       </div>
   )
 }
