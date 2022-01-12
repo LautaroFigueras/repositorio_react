4af6a8a97290dfdf3e7d3f514a0d9bab12471470
Lautaro Figueras <lautaromfigueras@gmail.com>
2022-01-11 22:06:15 -0300

    13

--- prueba-react/context/Cart.jsx
+++ prueba-react/context/Cart.jsx
@@ -1,18 +1,91 @@
 //import { useContext } from "react";
 import { useCartContext } from "./CartContext"
 
+import { addDoc, collection, doc, getFirestore, Timestamp, updateDoc, writeBatch } from "firebase/firestore"
+import { useState } from "react";
+
+
 
 function Cart() {
+    const [idOrder, setIdOrder] = useState('')
+    const [dataForm, setDataForm] = useState({
+        name:"", email:"", phone:""
+    })
+    const {cartList, borrarCarrito, precioTotal} = useCartContext()
 
-    const {cartList, borrarCarrito} = useCartContext()
+
+    const handleChange = (e) => {
+        setDataForm({
+            ...dataForm,
+                [e.target.name]: e.target.value
+        })
+    }    
+
+    console.log(dataForm)
+    const generarOrden = (e) =>{
+        e.preventDefault()
+
+        let orden = {}
+        orden.date = Timestamp.fromDate(new Date())
+
+        orden.buyer = dataForm
+        orden.total = precioTotal();
+
+        orden.items = cartList.map(cartItem => {
+            const id = cartItem.id;
+            const nombre = cartItem.name;
+            const precio = cartItem.price * cartItem.cantidad;
+            
+            return {id, nombre, precio}   
+        })
+
+        const db = getFirestore()
+        const ordenColeccion = collection(db, 'orders')
+        addDoc(ordenColeccion, orden)
+        .then(resp => setIdOrder(resp.id))
+        .catch(err => console.log(err))
+        .finally(()=> {
+            borrarCarrito()
+            setDataForm({
+                name:"", email:"", phone:""
+            })
+        })
+
+
+
+        const cleccionNoti = collection(db, 'items')
+        const queryActulizarStock = query(
+            cleccionNoti, where( documentId() , 'in', cartList.map(it => it.id))          
+        )
+
+        const batch = writeBatch(db)
+
+
+        getDocs(queryActulizarStock)
+        .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
+            stock: resp.data().stock - cartList.find(item => item.id === resp.id).cantidad
+        }) ))
+
+        batch.commit()
+
+
+    }
+
+
+
 
 
     return(
         <div>
+             {idOrder.length !== 0 && idOrder}
             {cartList.map(prod=> <li>{prod.name}  {prod.cantidad}</li>)}
+            <form
+                 onSubmit={generarOrden} 
+                onChange={handleChange} 
+            >
+            </form>
             <button onClick={borrarCarrito}>Vaciar Carrito</button>
             <button onClick={() => borrarItem (prod.id)} >Vaciar</button>
-
         </div>
     )
 }
